0c94c79207d7ce5e0922e6c47d5f03b6e6100260
Felipe da Hora <lipedahora2000@hotmail.com>
2023-11-08 21:00:09 -0300

    criacao routes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    let contador  = 0;
+</script>
+
+<h1>{contador}</h1>
+<input type="text">
+<button on:click={()=> contador++}>Aumentar</button>
+<button on:click={()=> contador--}>Diminuir</button>
